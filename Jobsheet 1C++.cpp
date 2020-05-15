298a70f242ff816985fe07417b19d6b143c1b5f5
diazkurniawan <64571423+diazkurniawan@users.noreply.github.com>
2020-05-15 14:06:23 +0800

    Create Jobsheet 1C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout<<"Nama : Diaz Dwi Kurniawan\n";
+	cout<<"NIM  : F1B019040\n";
+	cout<<"Kelompok : 8\n\n";
+	int nim[5]={5,6,8,9,10};
+	
+	cout<<"Nim ke-1 adalah "<<nim[0]<<endl;
+	cout<<"Nim ke-2 adalah "<<nim[1]<<endl;
+	cout<<"Nim ke-3 adalah "<<nim[2]<<endl;
+	cout<<"Nim ke-4 adalah "<<nim[3]<<endl;
+	cout<<"Nim ke-5 adalah "<<nim[4]<<endl;
+
+
+}
