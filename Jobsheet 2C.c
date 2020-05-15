04d4ed13ed11dcad4f0fce056e1183858546daa2
diazkurniawan <64571423+diazkurniawan@users.noreply.github.com>
2020-05-15 14:12:39 +0800

    Create Jobsheet 2C.c
    Mengisi dan menampilkan array (dinamis)

---
+++ Jobsheet 2C.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	printf("NAMA     : Diaz Dwi Kurniawan\n");
+	printf("NIM      : F1B019040\n");
+	printf("KELOMPOK : 8\n");
+	
+	int a [6];
+	
+	printf("Masukkan nilai 1 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 2 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 3 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 4 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 5 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 6 :");
+	scanf("%d",a);
+	
+}
