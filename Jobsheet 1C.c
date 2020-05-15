10588d83a42548bbb1e98e5c70f54ce171b388dd
diazkurniawan <64571423+diazkurniawan@users.noreply.github.com>
2020-05-15 14:08:53 +0800

    Create Jobsheet 1C.c
    Menampilkan array satu dimensi

---
+++ Jobsheet 1C.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+	
+	int myarray[10] = {1,23,17,4,-5,10,15,4,6,7};
+	
+	printf("\nNAMA		: Diaz Dwi Kurniawan");
+	printf("\nNIM		: F1B019040");
+	printf("\nKELOMPOK	: 8");
+	printf("\n\ndata ke-1 = %d" ,myarray[0]);
+	printf("\ndata ke-2 = %d" ,myarray[1]);
+	printf("\ndata ke-3 = %d" ,myarray[2]);
+	printf("\ndata ke-4 = %d" ,myarray[3]);
+	printf("\ndata ke-5 = %d" ,myarray[4]);
+	printf("\ndata ke-6 = %d" ,myarray[5]);
+	printf("\ndata ke-7 = %d" ,myarray[6]);
+	printf("\ndata ke-8 = %d" ,myarray[7]);
+	printf("\ndata ke-9 = %d" ,myarray[8]);
+	printf("\ndata ke-10 = %d" ,myarray[9]);
+}
