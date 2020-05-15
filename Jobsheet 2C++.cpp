2874419419a69ca881b10121eeda4074a812970d
diazkurniawan <64571423+diazkurniawan@users.noreply.github.com>
2020-05-15 14:11:06 +0800

    Create Jobsheet 2C++.cpp
    Mengisi dan menampilkan array (dinamis)

---
+++ Jobsheet 2C++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Nama     : Diaz Dwi Kurniawan"<<endl;
+	cout<<"NIM      : F1B019040"<<endl;
+	cout<<"Kelompok : 8"<<endl<<endl;
+	
+	int x [6];
+	
+	cout<<"Input nilai ke-1 : ";
+	cin>>x[0];
+	cout<<"Input nilai ke-2 : ";
+	cin>>x[1];
+	cout<<"Input nilai ke-3 : ";
+	cin>>x[2];
+	cout<<"Input nilai ke-4 : ";
+	cin>>x[3];
+	cout<<"Input nilai ke-5 : ";
+	cin>>x[4];
+	cout<<"Input nilai ke-6 : ";
+	cin>>x[5];
+	
+	system("pause");
+}
